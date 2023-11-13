ed6f7bdc1496626dd4c7931d6a643b87021561f8
Dani <fantastfad830@gmail.com>
2023-11-13 18:23:07 +0300

    initial

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SMSRuModule } from 'node-sms-ru/nestjs';
+import { SMSRu } from 'node-sms-ru';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    SMSRuModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (cfg: ConfigService) => ({
+        api_id: cfg.get<string>('API_KEY'),
+      }),
+    }),
+  ],
+  controllers: [AppController, SMSRu],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/app.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { SMSRu } from 'node-sms-ru';
+
+@Injectable()
+export class AppService {
+  constructor(private readonly sms: SMSRu) {}
+
+  async getHello() {
+    return await this.sms.sendSms({ to: '312312', msg: '23112' }); //Example
+  }
+}
