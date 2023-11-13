7779cca919cb33aa1e34bfa9acb4afabdca6015f
Dani <fantastfad830@gmail.com>
2023-11-14 00:03:19 +0300

    add tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,12 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SMSRuModule } from 'node-sms-ru/nestjs';
+import { Request, Response } from 'express';
 
 describe('AppController', () => {
   let appController: AppController;
 
+  const reqMock = {
+    query: {},
+  } as unknown as Request;
+
+  const statesResMap = {
+    send: jest.fn((x) => x),
+  };
+
+  const resMock = {
+    status: jest.fn((x) => statesResMap),
+    // send: jest.fn((y) => y),
+    send: jest.fn((x) => x),
+  } as unknown as Response;
+
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [SMSRuModule.forRoot({ api_id: process.env.API_KEY })],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
@@ -14,9 +31,26 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  it('Should be defined', () => {
+    expect(appController).toBeDefined();
+  });
+
+  describe('getAll', () => {
+    it('Should return a status of 400', () => {
+      appController.getAll(reqMock, resMock);
+      expect(resMock.status).toHaveBeenCalledWith(400);
+      expect(statesResMap.send).toHaveBeenCalledWith({
+        msg: 'Missing query param',
+      });
+    });
+
+    it('Should return a status of 200 when we are have some params and queries', () => {
+      reqMock.query = {
+        count: '10',
+        page: '2',
+      };
+      appController.getAll(reqMock, resMock);
+      expect(resMock.send).toHaveBeenCalledWith(200);
     });
   });
 });
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request, Response } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get() // Просто обратиться на path : http://localhost:3000
-  getHello() {
-    return this.appService.getHello();
+  @Get('all')
+  getAll(@Req() req: Request, @Res() res: Response) {
+    const { count, page } = req.query;
+    if (!count || !page) {
+      res.status(400).send({ msg: ' Missing query param' });
+    } else {
+      res.send(200);
+    }
   }
 }
